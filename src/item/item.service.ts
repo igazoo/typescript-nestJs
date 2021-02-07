1b88f12c5c664f7c6d7ec134ade0bc743ae77b71
igazoo <igazoo.23@gmail.com>
2021-02-07 16:58:23 +0900

    CRUDのロジック追加

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from "src/entities/item.entity";
-import { Repository } from "typeorm";
+import { Repository,InsertResult, UpdateResult, DeleteResult } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { CreateItemDTO } from "./item.dto";
 
 @Injectable()
 export class ItemService {
     constructor(
-        @InjectRepository(Item)    
+        @InjectRepository(Item)
         private readonly itemRepository: Repository<Item>
-    ){}
+    ) { }
+    
+    // テーブル全データ取得
+    async findAll(): Promise<Item[]>{
+        return await this.itemRepository.find();
+    }
+
+    // テーブルにアイテムを追加
+    async create(item: CreateItemDTO): Promise<InsertResult>{
+       return  await this.itemRepository.insert(item);
+    }
+
+    // idを指定し、１件のデータ取得
+    async find(id: number): Promise<Item>|null{
+        return await this.itemRepository.findOne({id: id});
+    }
+
+    // idを指定し、データの更新
+    async update(id: number, item): Promise<UpdateResult>{
+        return await this.itemRepository.update(id, item);
+    }
+
+    // idを指定し、データの削除
+    async delete(id: number): Promise<DeleteResult>{
+        return await this.itemRepository.delete(id);
+    }
 }
